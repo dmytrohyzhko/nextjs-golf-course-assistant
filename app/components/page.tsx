5c50faebcf51796c1cd5033ea581f8e4faa6867f
dmytrohyzhko <dmytrohyzhko@outlook.com>
2024-08-16 13:49:45 -0700

    fix: wrong path css file

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import styles from "../shared/page.module.css";
+import styles from "../examples/shared/page.module.css";
 import Chat from "../components/chat";
 import SlotsWidget from "../components/slots-widget";
 import { searchAvailability } from "../utils/availability";
